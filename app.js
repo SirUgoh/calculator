73797b23b2e6b92768e4ecb9257b4615269553e6
Nnamani Ugochukwu Frederick <sirugoh97@gmail.com>
2020-02-13 11:34:38 +0100

    functionality
    add all functions to app.js to improve functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const screen = document.querySelector(".screen");
 const btns = document.querySelector(".calc-btn");
 let buffer = "0";
+let operator;
 let runningTotal = 0;
 
 function handleNumber(value) {
@@ -11,12 +12,89 @@ function handleNumber(value) {
     }
 }
 
+function flushOperation(value) {
+    if (operator === "+") {
+        runningTotal += value;
+    }else if (operator === "−") {
+        runningTotal -= value;
+    }else if (operator === "×") {
+        runningTotal *= value;
+    }else if (operator === "÷") {
+        runningTotal /= value;
+    }else{
+        runningTotal %= value;
+    }
+}
+
+function handleMath(value) {
+    if (buffer === "0") {
+        //do nothing
+        return;
+    }
+    const intBuffer = +buffer;
+
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    }else{
+        flushOperation(intBuffer);
+    }
+    operator = value;
+
+    buffer = "0";
+}
+
+function handleSymbol(value) {
+    switch(value) {
+        case "C":
+            buffer = "0";
+            runningTotal = 0;
+            break;
+        case "←":
+            if (buffer.length === 1) {
+                buffer = "0";
+            }else{
+                buffer = buffer.substring(0, buffer.length - 1);
+            }
+            break;
+        case "=":
+            if (operator === null) {
+                //You need two numbers to do math
+                return;
+            }
+            flushOperation(+buffer);
+            operator = null;
+            buffer = +runningTotal;
+            runningTotal = 0;
+            break;
+        case ".":
+            if (buffer.includes(".")) {
+                //do nothing
+                return;
+            }else if (buffer === "0" || runningTotal !== false) {
+                buffer += ".";
+            }
+            break;
+        case "+":
+        case "−":
+        case "×":
+        case "÷":
+        case "%":
+            handleMath(value);
+            break;
+    }
+}
+
+function rerender() {
+    screen.innerText = buffer;
+}
+
 function buttonClick(value) {
     if (isNaN(parseInt(value))) {
         handleSymbol(value);
     }else{
         handleNumber(value);
     }
+    rerender();
 }
 
 function init() {
